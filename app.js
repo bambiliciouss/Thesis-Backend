9737243127c279250e8e2319bb8eab4403b65464
bambiliciouss <ninapotmendoza917@gmail.com>
2024-05-05 21:01:49 +0800

    Cors link

--- app.js
+++ app.js
@@ -24,8 +24,9 @@ app.use(cookieParser());
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://hydro-web-frontend.vercel.app/',
     credentials: true
 }));
 
