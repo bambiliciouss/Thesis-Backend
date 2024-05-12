8a506f8527357896196f7ae3eb2701863b75f18e
bambiliciouss <ninapotmendoza917@gmail.com>
2024-05-12 21:38:28 +0800

    Receipt

--- utils/eReceipt.js
+++ utils/eReceipt.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-module.exports = async (email, subject, user) => {
+module.exports = async (email, subject, order) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.HOST,
@@ -14,26 +14,206 @@ module.exports = async (email, subject, user) => {
     });
 
     const html = `
-    <html>
-    <head></head>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
+        <!-- Favicon -->
+        <link rel="icon" href="./images/favicon.png" type="image/x-icon" />
+    
+        <!-- Invoice styling -->
+        <style>
+            body {
+                font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+                text-align: center;
+                color: #777;
+            }
+    
+            body h1 {
+                font-weight: 300;
+                margin-bottom: 0px;
+                padding-bottom: 0px;
+                color: #000;
+            }
+    
+            body h3 {
+                font-weight: 300;
+                margin-top: 10px;
+                margin-bottom: 20px;
+                font-style: italic;
+                color: #555;
+            }
+    
+            body a {
+                color: #06f;
+            }
+    
+            .invoice-box {
+                max-width: 800px;
+                margin: auto;
+                padding: 30px;
+                border: 1px solid #eee;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+                font-size: 16px;
+                line-height: 24px;
+                font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+                color: #555;
+            }
+    
+            .invoice-box table {
+                width: 100%;
+                line-height: inherit;
+                text-align: left;
+                border-collapse: collapse;
+            }
+    
+            .invoice-box table td {
+                padding: 5px;
+                vertical-align: top;
+            }
+    
+            .invoice-box table tr td:nth-child(2) {
+                text-align: right;
+            }
+    
+            .invoice-box table tr.top table td {
+                padding-bottom: 20px;
+            }
+    
+            .invoice-box table tr.top table td.title {
+                font-size: 45px;
+                line-height: 45px;
+                color: #333;
+            }
+    
+            .invoice-box table tr.information table td {
+                padding-bottom: 40px;
+            }
+    
+            .invoice-box table tr.heading td {
+                background: #eee;
+                border-bottom: 1px solid #ddd;
+                font-weight: bold;
+            }
+    
+            .invoice-box table tr.details td {
+                padding-bottom: 20px;
+            }
+    
+            .invoice-box table tr.item td {
+                border-bottom: 1px solid #eee;
+            }
+    
+            .invoice-box table tr.item.last td {
+                border-bottom: none;
+            }
+    
+            .invoice-box table tr.total td:nth-child(2) {
+                border-top: 2px solid #eee;
+                font-weight: bold;
+            }
+    
+            @media only screen and (max-width: 600px) {
+                .invoice-box table tr.top table td {
+                    width: 100%;
+                    display: block;
+                    text-align: center;
+                }
+    
+                .invoice-box table tr.information table td {
+                    width: 100%;
+                    display: block;
+                    text-align: center;
+                }
+            }
+        </style>
+    </head>
     <body>
-      <div style="background-color: #f2f2f2; padding: 20px; width: 80%; max-width: 600px; margin: 20px auto;">
-        <div style="background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
-          <div style="text-align: center; margin-bottom: 20px;">
-            <div style="background-color: #9bbffa; border-radius: 5px; padding: 10px; height: auto;">
-              <h1>${subject}</h1>
-            </div>
-          </div>
-          
-          <p>Hi, </p>
-          <p>Thank you for using our service. To reset your password, please click the link below:</p>
-          <div style="text-align: center;">
-           
-          </div>
+        <div class="invoice-box">
+            <table>
+                <tr class="top">
+                    <td colspan="2">
+                        <table>
+                            <tr>
+                                <td>
+                                    ${order.selectedStore.branchNo} <br/>
+                                    ${order.selectedStore.address}
+                                </td>
+    
+                                <td>
+                                    Invoice #: ${order._id}<br />
+                                    Created: ${new Date(order.createdAt).toLocaleString()}
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+    
+                <tr class="information">
+                    <td colspan="2">
+                        <table>
+                            <tr>
+                                <td>
+                                    Delivery Address: <br/>
+                                    ${order.deliveryAddress.houseNo}, ${order.deliveryAddress.purokNum} <br/>
+                                    ${order.deliveryAddress.streetName}, ${order.deliveryAddress.barangay} <br/>
+                                    ${order.deliveryAddress.city}
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+    
+                <tr class="heading">
+                    <td>Payment Method</td>
+                </tr>
+    
+                <tr class="details">
+                    ${order.orderStatus.map(status => `
+                        <td>  
+                            <li>${status.orderLevel} - ${new Date(status.datedAt).toLocaleString()}</li>
+                            ${status.staff ? `
+                                <ul>
+                                    <li>Staff: ${status.staff.fname} ${status.staff.lname}</li>
+                                    <li>Role: ${status.staff.role}</li>
+                                </ul>
+                            ` : ''}
+                        </td>
+                    `).join('')}
+                </tr>
+    
+                <tr class="heading">
+                    <td>Item</td>
+                    <td>Quantity</td>
+                    <td>Price</td>
+                    <td>Amount</td>
+                </tr>
+    
+                ${order.orderItems.map(item => `
+                    <tr class="item">
+                        <td>${item.type}</td>
+                        <td>${item.quantity}</td>
+                        <td>₱ ${item.price}</td>
+                        <td>${item.quantity * item.price}</td>
+                    </tr>
+                `).join('')}
+    
+                <tr class="item last">
+                    <td>Delivery Fee</td>
+                    <td>₱ ${order.selectedStore.deliveryFee}</td>
+                </tr>
+    
+                <tr class="total">
+                    <td></td>
+                    <td>Total: ₱ ${order.totalPrice}</td>
+                </tr>
+            </table>
         </div>
-      </div>
     </body>
     </html>
+    
     `;
 
     const mailOptions = {
