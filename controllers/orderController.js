507aa7240d8a4820e7b3f1db0d99d09941379d02
bambiliciouss <ninapotmendoza917@gmail.com>
2024-06-03 23:17:13 +0800

    order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -107,26 +107,26 @@ exports.newOrder = async (req, res, next) => {
     }
   }
 
-  const paymongoToken = await new PaymongoToken({
-    orderId: order._id,
-    token: crypto.randomBytes(32).toString("hex"),
-    verificationTokenExpire: new Date(Date.now() + 2 * 60 * 1000),
-  }).save();
+  // const paymongoToken = await new PaymongoToken({
+  //   orderId: order._id,
+  //   token: crypto.randomBytes(32).toString("hex"),
+  //   verificationTokenExpire: new Date(Date.now() + 2 * 60 * 1000),
+  // }).save();
 
-  if (req.body.paymentInfo === "GCash") {
-    const temporaryLink = `${process.env.BASE_URL}/paymongo-gcash/${paymongoToken.token}/${order._id}`;
-    console.log();
+  // if (req.body.paymentInfo === "GCash") {
+  //   const temporaryLink = `${process.env.BASE_URL}/paymongo-gcash/${paymongoToken.token}/${order._id}`;
+  //   console.log();
 
-    const checkoutUrl = await handlePayMongo(
-      //order.orderProducts,
-      order.orderItems,
-      temporaryLink
-    );
+  //   const checkoutUrl = await handlePayMongo(
+  //     //order.orderProducts,
+  //     order.orderItems,
+  //     temporaryLink
+  //   );
 
-    console.log(checkoutUrl, "checkout");
+  //   console.log(checkoutUrl, "checkout");
 
-    return res.json({ checkoutUrl });
-  }
+  //   return res.json({ checkoutUrl });
+  // }
 
   console.log(order);
 
@@ -1030,5 +1030,4 @@ exports.allOrdersRiderOutforDelivery = async (req, res, next) => {
   }
 };
 
-// exports.newOrder = async (req, res, next) => {
-// };
+
